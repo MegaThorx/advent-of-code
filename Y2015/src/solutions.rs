6d44b10c4a05865cd616dd546d0760e39b21ee95
MegaThorx <git@merx.dev>
2024-12-02 13:38:46 +0100

    Add solution for Y2015D04

--- Y2015/src/solutions.rs
+++ Y2015/src/solutions.rs
@@ -2,10 +2,12 @@
 use crate::solutions::day1::Day1;
 use crate::solutions::day2::Day2;
 use crate::solutions::day3::Day3;
+use crate::solutions::day4::Day4;
 
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 pub struct Solutions {}
 
@@ -15,6 +17,7 @@ impl SolverFactory for Solutions {
             1 => Some(Box::new(Day1 {})),
             2 => Some(Box::new(Day2 {})),
             3 => Some(Box::new(Day3 {})),
+            4 => Some(Box::new(Day4 {})),
             _ => None,
         }
     }
